53e46efb2d65f17c06160c07c1d5a4e3ed11baa2
Winetq <winetq9679@gmail.com>
2021-09-07 22:01:04 +0200

    Find second largest element in an array

--- src/main/java/com/epam/jap/Main.java
+++ src/main/java/com/epam/jap/Main.java
@@ -1,23 +1,77 @@
 package com.epam.jap;
 
-import com.epam.jap.user.User;
-import com.epam.jap.user.UserRegistry;
-
-import java.util.List;
+import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        UserRegistry userRegistry = new UserRegistry();
-        userRegistry.registerUser(
-                new User("Marian", "Kajtek", "123456789", "marian@gmail.com")
-        );
-        userRegistry.registerUsers(List.of(
-                new User("Janusz", "Kowalski", "111222333", "majtek@gmail.com"),
-                new User("Majtek", "Kajtek", "987654321", "majtek@gmail.com"),
-                new User("Mariusz", "Kowal", "111222333", "@gmail.com")
-        ));
-        System.out.println(userRegistry.getUserByEmail("majtek@gmail.com"));
-        userRegistry.show();
+        int[] a = {12, 35, 1, 10, 34, 1};
+        int[] b = {44, 66, 99, 77, 33, 22, 55};
+        int[] c = {1, 2, 5, 6, 3, 2};
+
+        /**
+         * Time Complexity = O(n + n * log(n)) = O(n * log(n))
+         * Space Complexity = O(n)
+         */
+        System.out.println("Solution 1:");
+        System.out.println(solution1(a, a.length));
+        System.out.println(solution1(b, b.length));
+        System.out.println(solution1(c, c.length));
+
+        /**
+         * Time Complexity = O(n + n) = O(n)
+         * Space Complexity = O(1)
+         */
+        System.out.println("Solution 2:");
+        System.out.println(solution2(a, a.length));
+        System.out.println(solution2(b, b.length));
+        System.out.println(solution2(c, c.length));
+
+        /**
+         * Time Complexity = O(n)
+         * Space Complexity = O(1)
+         */
+        System.out.println("Solution 3:");
+        System.out.println(solution3(a, a.length));
+        System.out.println(solution3(b, b.length));
+        System.out.println(solution3(c, c.length));
+    }
+
+    private static int solution1(int[] arr, int n) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = arr[i];
+        }
+        Arrays.sort(ans);
+        return ans[n-2];
+    }
+
+    private static int solution2(int[] arr, int n) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int i : arr) {
+            first = Math.max(first, i);
+        }
+        for (int i : arr) {
+            if (i != first) {
+                second = Math.max(second, i);
+            }
+        }
+        return second;
+    }
+
+    private static int solution3(int[] arr, int n) {
+        int first = Integer.MIN_VALUE;
+        int second = Integer.MIN_VALUE;
+        for (int i : arr) {
+            if (i > first) {
+                second = first;
+                first = i;
+            }
+            if (i > second && i != first) {
+                second = i;
+            }
+        }
+        return second;
     }
 }
 
